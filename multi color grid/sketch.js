b5b9b8d8e62706e25733727071173339078224e6
TylerLusher <tyjojo.tl@gmail.com>
2023-09-26 12:06:40 -0600

    sept 26 2023

---
+++ multi color grid/sketch.js
@@ -0,0 +1,23 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(220);
+}
+function keyPressed (){
+  circle(mouseX,mouseY,50);
+  if ( key === ' '){
+    background(220);
+  }
+  
+}
+
+function draw() {
+  
+}
