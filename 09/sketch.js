1d65328a821819cb53e0cac3c15bb76ba427aa3c
TylerLusher <tyjojo.tl@gmail.com>
2023-10-02 12:16:39 -0600

    oct 2 2023

---
+++ 09/sketch.js
@@ -0,0 +1,28 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+//global variables 
+let balls = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  //background(220);
+}
+
+class Balls{
+  // class constructor and properties 
+  constructor(yPosition,color){
+    this.xPosition = 0;
+    this.yPosition;
+    this.xSpeed = random(3-15);
+    this.color;
+  }
+  // class methods/functions 
+}
