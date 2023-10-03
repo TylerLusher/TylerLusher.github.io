8d2ffa05fea8039ef7348a9eb037e052ed539aba
TylerLusher <tyjojo.tl@gmail.com>
2023-10-03 12:17:39 -0600

    Bruh the second

--- grid/sketch.js
+++ grid/sketch.js
@@ -6,7 +6,7 @@
 // - describe what you did to take this project "above and beyond"
 
 // Global Variables
-
+let sizes = 10;
 
 
 
@@ -16,5 +16,43 @@ function setup() {
 }
 
 function draw() {
+  print(sizes);
+  if (mouseIsPressed === true){
+    if(mouseButton === RIGHT){
+      if (sizes>= 9){
+        sizes-=1;
+      }
+    }
+    else{    
+      sizes+=1;
+    }
+    grid();
+  }
+}
+
+function keyPressed(){
+  grid();
+}
+
+function grid(){
   background(220);
+  let i = Math.floor(random(4));
+  for(let x =0; x+sizes<width; x=x+sizes){
+    for(let y=0; y+sizes<height; y=y+sizes){
+      colors(i);
+      square(x,y,sizes);
+    }
+  }
+}
+
+function colors(i){
+  if (i === 1){
+    fill(random(255),0,0);
+  }
+  else if (i === 2){
+    fill(0,random(255),0);
+  }
+  else{
+    fill(0,0,random(255));
+  }
 }
