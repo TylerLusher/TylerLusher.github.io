7edde5df53f6566db88d99a6b64f05ecf264498d
TylerLusher <tyjojo.tl@gmail.com>
2023-10-06 10:44:28 -0600

    bruh

--- Noise/sketch.js
+++ Noise/sketch.js
@@ -15,31 +15,32 @@ const noiseShift = 0.01;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CORNERS);
-  fill(0);
   rectPerlen();
-  flag();
 }
 
-function flag(){
-  square(xPos,height-highestY,5);
-  rect(xPos,height-highestY,xPos+4,rectHeight);
+function flag(x,y){
+  fill(100);
+  rect(x,y,x+0.5,y-20);
+  triangle(x,y-21,x,y-10,x+10,y-15);
 }
 
 function rectPerlen(){
+  fill(0);
   strokeWeight(2);
   stroke(0);
-  highestY=0;
+  highestY=height;
+  xPos=0;
   for(let x = 0; x<width;x=x+0.5){
     rectHeight = noise(noisePositionRound);
     rectHeight = map(rectHeight,0,1,0,height);
     noisePositionRound += noiseShift;
     rect(x,height,0.5+x,rectHeight);
-    if(highestY<rectHeight){
+    if(highestY>rectHeight){
       highestY=rectHeight;
       xPos=x;
     }
   }
-
+  flag(xPos,highestY);
 }
 
 
