0e44a1552c437b56810d077185c8e8e9f7990883
TylerLusher <tyjojo.tl@gmail.com>
2023-10-19 11:24:39 -0600

    oct33

--- CarsCarsCars/sketch.js
+++ CarsCarsCars/sketch.js
@@ -9,10 +9,24 @@ function setup() {
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  drawRoad();
 }
 
 function drawRoad(){
+  fill(0);
+  rect(0,height/2 -100,width,200);
+  colorMode(RGB,100);
+  stroke(255,255,0);
+  for(let lines=0;lines<width; lines=lines+50){
+    line(lines,height/2,lines+40,height/2);
+  }
+}
 
+class Cars{
+  constructor(){
+
+    
+  }
 
 }
