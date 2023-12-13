63731cf6261a0560863e84ce3f465e52099608ed
TylerLusher <tyjojo.tl@gmail.com>
2023-12-13 12:17:11 -0600

    More on the capstone
    Today Ive separated the the tower and bullet classes and added the map and round class

--- cap/sketch.js
+++ cap/sketch.js
@@ -1,57 +1,105 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 let array=[];
-let ab=[];
+let ar;
+let theMap;
+let RB;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ar=new Rounds(40);
+  theMap=new Maps();
+  RB=new Ballon();
 }
 
 function draw() {
   background(220);
-  for (let hi of array){
-    hi.display();
-  }
+  theMap.display();
 }
 
 function mousePressed(){
   if (key === "w"){
-    array.push(new Tower(mouseX,mouseY,ab.push(new Bullet(this.x,this.y))));
+    array.push(new Tower(mouseX,mouseY));
+  }
+  if (key === "b"){
+    
   }
 }
 
-class Tower{
-  constructor(x,y,b){
-    this.x=x;
-    this.y=y;
-    this.bt=b;
+class Maps{
+  constructor(){
+
   }
 
   display(){
-    circle(this.x,this.y);
-    this.bt.display();
-    this.bt.move();
+    rect(-5,height/2,width,20);
+    for (let hi of array){
+      hi.display();
+    }
+  }
+
+}
+
+class Tower{
+  constructor(x,y){
+    this.x=x;
+    this.y=y;
+  }
+
+  display(){
+    circle(this.x,this.y,5);
   }
 }
 
 class Bullet{
-  constructor(x,y){
-    this.bx=x;
-    this.by=y;
-    this.x=mouseX;
-    this.y=mouseY;
+  constructor(d,f){
+    this.bx=d;
+    this.by=f;
   }
   move(){
-    if (this.x>this.bx){
-      //bruh
+    if (mouseX>this.bx){
+      this.bx+=1;
+    }
+    if (mouseX<this.bx){
+      this.bx-=1;
+    }
+    if (mouseY>this.by){
+      this.by+=1;
+    }
+    if (mouseY<this.by){
+      this.by-=1;
     }
   }
   display(){
-    rect(this.bx,this.by,5,10);
+    circle(this.bx,this.by,2.5);
+    this.move();
   }
 }
 
+class Ballon{
+  constructor(d,f,r){
+    this.bHp;
+    this.baX;
+    this.baY;
+    this.bxs=d;
+    this.bys=f;
+    this.round=r;
+  }
+  move(){
+
+  }
+  display(){
+
+  }
+  ballonHp(){
+
+  }
+}
+
+class Rounds{
+  constructor(r){
+    this.round=r;
+  }
+  display(){
+
+  }
+
+}
