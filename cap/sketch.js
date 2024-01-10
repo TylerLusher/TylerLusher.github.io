31bce399b1c18386c2d93ca692166092a62f577d
TylerLusher <tyjojo.tl@gmail.com>
2024-01-10 12:17:01 -0600

    capstone
    today i implemented the movement of the ballon as a class

--- cap/sketch.js
+++ cap/sketch.js
@@ -2,11 +2,17 @@
 //
 //  _  = 1
 //
+//  left  _ = 7
+//
 //  |  = 2
+//
+//  going down =9
 //
 //  |_ = 3
 //   _
 //  |  = 4
+//
+//  going the other way =8
 //  _
 //   | = 5
 //
@@ -14,10 +20,10 @@
 let maps=[
   [0,0,0,0,0,0,0,0,0,0],
   [0,0,4,1,1,1,1,5,0,0],
-  [0,0,2,0,0,0,0,2,0,0],
-  [1,1,6,0,0,0,0,2,0,0], 
-  [0,0,0,0,0,4,1,6,0,0],
-  [0,0,0,0,0,2,0,0,0,0],
+  [0,0,2,0,0,0,0,9,0,0],
+  [1,1,6,0,0,0,0,9,0,0], 
+  [0,0,0,0,0,8,7,6,0,0],
+  [0,0,0,0,0,9,0,0,0,0],
   [0,0,0,0,0,3,1,1,1,1],
   [0,0,0,0,0,0,0,0,0,0],
 ];
@@ -48,27 +54,118 @@ function startGame(){
   ballon.push(new Ballon(0,350,"right"));
 }
 
-function moveBallon(x){
-  if (maps[x[0]][x[1]]===1){
-    //
-  }
-}
-
 class Ballon{
   constructor(x,y,DtoMove){
     //this.cords=start;
     this.mD=DtoMove;
     this.bx=x;
     this.by=y;
+    this.count=0;
   }
+
   display(){
     circle(this.bx,this.by,10);
   }
-  move(){
+
+  move(){ // i think i can combine this and the check square
     if(this.mD==="right"){
-      //
+      this.bx+=1;
+      this.count+=1;
+      if (this.count===100){
+        this.mD=this.checkSquare(this.bx,this.by-50);
+        print(this.mD);
+      }
     }
+    if(this.mD==="LeftCornerUp"){
+      if(this.count>=50&&this.count!==100){
+        this.by-=1;
+        this.count+=1;
+      }
+      else if(this.count<50){
+        this.count+=1;
+        this.bx+=1;
+      }
+      if(this.count===100){
+        this.mD=this.checkSquare(this.bx-50,this.by-100);
+        print(this.mD);
+      }
+    }
+    if(this.mD==="strightUp"){
+      this.by-=1;
+      this.count+=1;
+      if(this.count===100){
+        this.mD=this.checkSquare(this.bx-50,this.by-100);
+        print(this.mD);
+      }
+    }
+    if(this.mD==="DownCornerRight"){
+      if(this.count>=50&&this.count!==100){
+        this.bx+=1;
+        this.count+=1;
+      }
+      else if(this.count<50){
+        this.count+=1;
+        this.by-=1;
+      }
+      if(this.count===100){
+        this.mD=this.checkSquare(this.bx,this.by-50);
+        print(this.mD);
+      }
+    }
+    if(this.mD==="LeftCornerDown"){
+      if(this.count>=50&&this.count!==100){
+        this.by+=1;
+        this.count+=1;
+      }
+      else if(this.count<50){
+        this.count+=1;
+        this.bx+=1;
+      }
+
+      if(this.count===100){
+        this.mD=this.checkSquare(this.bx-50,this.by);
+        print(this.mD);
+      }
+    }
+  } 
+  
+  checkSquare(xx,yy){
+    print(xx);
+    print(yy);
+    this.count=0;
+    if (maps[yy/100][xx/100]===0){
+      return "blank";
+    }
+    if (maps[yy/100][xx/100]===1){
+      return "right";
+    }
+    if (maps[yy/100][xx/100]===2){
+      return "strightUp";
+    }
+    if (maps[yy/100][xx/100]===3){
+      return "cornerLeft";
+    }
+    if (maps[yy/100][xx/100]===4){
+      return "DownCornerRight";
+    }
+    if (maps[yy/100][xx/100]===5){
+      return "LeftCornerDown";
+    }
+    if (maps[yy/100][xx/100]===6){
+      return "LeftCornerUp";
+    }
+    if (maps[yy/100][xx/100]===7){
+      return "Left";
+    }
+    if (maps[yy/100][xx/100]===8){
+      return "RightCornerDown";
+    }
+    if (maps[yy/100][xx/100]===9){
+      return "Down";
+    }
+    return "bruh";
   }
+
 }
 
 
