c694dee0aafa06060f31271ac4bedb4f6544dd08
TylerLusher <tyjojo.tl@gmail.com>
2024-01-23 03:28:16 -0600

    capstone
    just some small improvements lol

--- cap/sketch.js
+++ cap/sketch.js
@@ -40,40 +40,83 @@ let maps=[
   [0,0,0,0,0,3,1,1,1,1],
   [0,0,0,0,0,0,0,0,0,0],
 ];
-
+let rounds =[[1,1,1],[2,2,2],[1,2,1,2],[2,2,2,2],[5,5]];
 function setup() {
   createCanvas(1000,800);
   startGame();
-  //print("hi");
   frameRate(60);
 }
 
 let towers=[];
 let ballon=[];
 let game;
-let countFrames=0;
+let count=-1;
+let gameStart=0;
+let wave=0;
+let wb=0;
 function draw(){
-  countFrames+=1;
   background(100);
-  drawMap();
-  let count=-1;
-  count=-1;
-  for(let a of ballon){
-    count++;
-    if (a!==undefined){
-      a.display();
-      a.move();
-      a.checkIfBallonIsOff(count);
-      if(countFrames%60===0){
-        a.checkIfTowerIsNears(count);
-      }
+  if (gameStart===0){
+    fill(0);
+    textSize(50);
+    text("Tower defense",(width/2)-100,(height/2)-100);
+    fill(255);
+    rect(475,330,100,50);
+    fill(0);
+    textSize(25);
+    text("start",494,358);
+    if (mouseX>=476&&mouseX<=574&&mouseY>=332&&mouseY<=376 && mouseIsPressed){
+      gameStart=1;
     }
   }
-  game.draw();
+  else{
+    drawMap();
+    fill(0);
+    textSize(50);
+    text("wave " +wave+ " of 5",300,(height)-50)
+    count=-1;
+    for(let a of ballon){
+      count++;
+      a.display();
+      a.move();
+      a.checkIfTowerIsNears(count,a);
+      a.checkIfBallonIsOff(count);
+    }
+    count=-1;
+    game.draw();
+    if(frameCount%60===0){
+      for (let a of towers){
+        a.shootset();
+      }
+    }
+    if(frameCount%30===0){
+      if (rounds[wave]===undefined&&ballon[0]===undefined){
+        fill(0);
+        textSize(25);
+        background(200);
+        text("You win",494,358);
+        noLoop();
+      }
+      else{ 
+        if (rounds[wave].length<wb+1 && ballon[0]===undefined){
+          wb=0;
+          wave++;
+        }
+        else if (rounds[wave].length!==wb){
+          ballon.push(new Ballon(rounds[wave][wb]));
+          wb++
+        }
+      }
+    }
+    
+    if(game.gameEndCheck()===true){
+      noLoop();
+    }
+  }
 }
 
 function startGame(){
-  ballon.push(new Ballon(0,350,1));
+  //ballon.push(new Ballon(0,350,1));
   game = new Game();
 }
 
@@ -86,16 +129,25 @@ class Game{
     this.hp-=1;
     print(this.hp);
   }
+  gameEndCheck(){
+    if(this.hp<=0){
+      fill(0);
+      textSize(50);
+      text("you lose press f5 to restart",(width/2)-100,height/2);
+      return true;
+    }
+    return false;
+  }
   towerPlaced(){
     let xCount=0;
     let yCount=0;
     for (let y=0; y<800; y=y+100){
       for (let x=0; x<1000; x=x+100){
         if (mouseX>=x && mouseX<=x+100 && mouseY >=y && mouseY <=y+100 && maps[yCount][xCount]===0){
-          if (this.cash>=100){
+          if (this.cash>=50){
             towers.push(new Tower(yCount,xCount));
             maps[yCount][xCount]=11;
-            this.cash-=100;
+            this.cash-=50;
           }
         }
         xCount++;
@@ -110,7 +162,8 @@ class Game{
   draw(){
     fill(0);
     textSize(50);
-    text(this.cash,50,50);
+    text(this.cash+"$",150,(height)-50);
+    text(this.hp+"<3",50,(height)-50);
   }
 }
 
@@ -118,38 +171,51 @@ class Tower{
   constructor(y,x){
     this.x=x;
     this.y=y;
+    this.shoot=0;
   }
-  checkIfTowerIsNear(q,w){
+  shootset(){
+    this.shoot=0;
+  }
+  checkIfTowerIsNear(q,w,c){
     let nums=[-1,0,1];
+    if (this.shoot===0 && c !==0){
     for (let a of nums){
       if (this.x===q-(a)){
         if(this.y===w-1){
+          this.shoot=1;
           return true;
         }
         if(this.y===w){
+          this.shoot=1;
           return true;
         }
         if(this.y===w+1){
+          this.shoot=1;
           return true;
         }
       }
     }
+    }
+    else{
+      return false;
+    }
   }
 }
-// make tower an acutle class that is implemented 
 
 class Ballon{
-  constructor(x,y,DtoMove){
-    this.mD=DtoMove;
-    this.bx=x;
-    this.by=y;
+  constructor(hp){
+    this.mD=1;
+    this.bx=0;
+    this.by=350;
     this.count=0;
     this.posX;
     this.posY;
-    this.bhp=5;
+    this.bhp=hp;
+    this.ogbhp=hp;
   }
 
   display(){
+    fill(255,0,0);
     circle(this.bx,this.by,10);
   }
 
@@ -313,16 +379,15 @@ class Ballon{
     }
   } 
 
-  checkIfTowerIsNears(z){
+  checkIfTowerIsNears(z,c){
     for(let a of towers){
-      if (a.checkIfTowerIsNear(this.posX,this.posY)){
+      if (a.checkIfTowerIsNear(this.posX,this.posY,this.bhp)){
         this.bhp-=1;
         print(this.bhp);
         if (this.bhp===0){
-          game.money(5);
-          ballon[z]=undefined;
+          game.money(5*this.ogbhp);
+          ballon.splice(z,1);
         }
-        
       }
     }
   }
@@ -332,9 +397,11 @@ function mousePressed(){
   if (key === "t"){
     game.towerPlaced();
   }
+  /*
   if (key === "b"){
     ballon.push(new Ballon(0,350,1));
   }
+  */
 }
 
 function drawMap(){
@@ -345,6 +412,10 @@ function drawMap(){
       if (maps[yCount][xCount]===0){
         fill(255);
         square(x,y,100);
+      }
+      if (maps[yCount][xCount]===11){
+        fill(0,255,0);
+        circle(x+50,y+50,50);
       }
       xCount++;
     }
